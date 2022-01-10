d99479ed7d0ae0cbd10c349ed4b170b5480a18e0
JoaoVGStahl <jvitorstahl@hotmail.com>
2022-01-10 11:03:17 -0300

    add bootstrap into project

--- ProjetoFinal/frontend/src/app/app.module.ts
+++ ProjetoFinal/frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { NavegacaoModule } from './navegacao/navegacao.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NavegacaoModule
+    NavegacaoModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ProjetoFinal/frontend/src/app/navegacao/navegacao.module.ts
+++ ProjetoFinal/frontend/src/app/navegacao/navegacao.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [
@@ -16,7 +19,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
     ],
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        NgbModule
     ],
     exports: [
         MenuComponent,
